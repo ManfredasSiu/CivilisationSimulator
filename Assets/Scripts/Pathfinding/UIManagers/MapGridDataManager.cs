1c7591f35ffdaf4f944287f041fbe11c5e8aee26
ManfredasŠiurkus <manfredas.surkus@unity3d.com>
2022-09-29 15:53:17 +0300

    Added missing files

---
+++ Assets/Scripts/Pathfinding/UIManagers/MapGridDataManager.cs
@@ -0,0 +1,36 @@
+﻿
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace PathFinding.Scripts.UIManagers
+{
+    public class MapGridDataManager : MonoBehaviour
+    {
+        [SerializeField]
+        ObjectMap m_ColliderObjectContainer;
+        
+        [SerializeField]
+        GameObject m_BoundingBoxContainer;
+
+        [SerializeField]
+        Tilemap m_PathTileMap;
+
+        [SerializeField] 
+        TileScriptableObject m_TileStructuresContainer;
+
+        [SerializeField]
+        int m_MapWidth;
+        
+        [SerializeField]
+        int m_MapHeight;
+
+        public int mapWidth => m_MapWidth;
+        public int mapHeight => m_MapHeight;
+        
+        public ObjectMap colliderObjectContainer => m_ColliderObjectContainer;
+        public GameObject boundingBoxContainer => m_BoundingBoxContainer;
+        public Tilemap pathTileMap => m_PathTileMap;
+        public TileScriptableObject tileStructuresContainer => m_TileStructuresContainer;
+    }
+}
